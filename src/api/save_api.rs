11a6b4d1d7ae7d939b1a5cf217c90e46fa096947
Colin Swenson-Healey <chozandrias76@github.com>
2024-08-04 17:37:16 -0700

    refactor: create sub module for save data api on save api

--- src/api/save_api.rs
+++ src/api/save_api.rs
@@ -1,3 +1,5 @@
+pub mod save_data_api;
+
 use std::{
     collections::{BTreeMap, HashMap},
     num::ParseIntError,
@@ -55,31 +57,6 @@ impl SaveApi {
         SaveApi { raw: save }
     }
 
-    /// Converts the save data to a vector of bytes.
-    ///
-    /// # Example
-    /// ```rust
-    /// use er_save_lib::SaveApi;
-    /// let save_api = SaveApi::from_path("./test/ER0000.sl2").unwrap();
-    /// let bytes = save_api.to_vec().unwrap();
-    /// ```
-    pub fn to_vec(&self) -> Result<Vec<u8>, SaveApiError> {
-        let bytes = self.raw.write_to_vec()?;
-        Ok(bytes)
-    }
-
-    /// Writes the save data to the specified path.
-    ///
-    /// # Example
-    /// ```rust
-    /// use er_save_lib::SaveApi;
-    /// let save_api = SaveApi::from_path("./test/ER0000.sl2").unwrap();
-    /// save_api.write_to_path("./test/null.sl2").unwrap();
-    /// ```
-    pub fn write_to_path(&self, path: impl AsRef<Path>) -> Result<(), SaveApiError> {
-        Ok(self.raw.write_to_path(path)?)
-    }
-
     /// Creates a `SaveApi` instance from a slice of bytes.
     ///
     /// # Example
@@ -104,24 +81,9 @@ impl SaveApi {
         let raw = Save::from_path(path)?;
         Ok(SaveApi { raw })
     }
+}
 
-    /// Returns the platform type of the save file.
-    ///
-    /// # Example
-    /// ```rust
-    /// use er_save_lib::{SaveApi, SaveType};
-    /// let save_api = SaveApi::from_path("./test/ER0000.sl2").unwrap();
-    /// let platform = save_api.platform();
-    /// assert_eq!(platform, SaveType::PC);
-    /// ```
-    pub fn platform(&self) -> SaveType {
-        if self.raw.header.len() == 0x6c {
-            SaveType::Playstation
-        } else {
-            SaveType::PC
-        }
-    }
-
+impl SaveApi { // TODO: Move most of these into save_api/user_data
     /// Returns the index of the character with the given name.
     ///
     /// # Example
@@ -207,31 +169,6 @@ impl SaveApi {
         on: bool,
     ) -> Result<(), SaveApiError> {
         EventFlagsApi::set_event_flag(&mut self.raw, event_id, character_index, on)
-    }
-
-    /// Returns the Steam ID associated with the save file.
-    ///
-    /// # Example
-    /// ```rust
-    /// use er_save_lib::SaveApi;
-    /// let save_api = SaveApi::from_path("./test/ER0000.sl2").unwrap();
-    /// let steam_id = save_api.steam_id();
-    /// ```
-    pub fn steam_id(&self) -> u64 {
-        self.raw.user_data_10.steam_id
-    }
-
-    /// Sets the Steam ID associated with the save file.
-    ///
-    /// # Example
-    /// ```rust
-    /// use er_save_lib::SaveApi;
-    /// let mut save_api = SaveApi::from_path("./test/ER0000.sl2").unwrap();
-    /// save_api.set_steam_id(1234567890).unwrap();
-    /// ```
-    pub fn set_steam_id(&mut self, steam_id: u64) -> Result<(), SaveApiError> {
-        self.raw.user_data_10.steam_id = steam_id;
-        Ok(())
     }
 
     /// Returns the name of the character at the specified index.
@@ -356,14 +293,18 @@ impl SaveApi {
     }
 
     /// Sets the equipped gestures for the character at the specified index.
-    /// 
+    ///
     /// # Example
     /// ```rust
     /// use er_save_lib::SaveApi;
     /// let mut save_api = SaveApi::from_path("./test/ER0000.sl2").unwrap();
     /// save_api.set_equipped_gestures(0, vec![1u32,2u32,3u32]).unwrap();
     /// ```
-    pub fn set_equipped_gestures(&mut self, index: usize, new_gestures: Vec<u32>) -> Result<(), SaveApiError> {
+    pub fn set_equipped_gestures(
+        &mut self,
+        index: usize,
+        new_gestures: Vec<u32>,
+    ) -> Result<(), SaveApiError> {
         self.raw.user_data_x[index]
             .equipped_gestures
             .equipped_gesture = new_gestures;
